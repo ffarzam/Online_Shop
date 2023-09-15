775051941e5889f81decfce66116c9a14f6a94a8
Farzam <ffarzam_1992@yahoo.com>
2023-09-15 16:18:31 +0330

    chore add products app models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Brand(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Feature(models.Model):
+    color = models.CharField(max_length=25)
+    ram = models.PositiveIntegerField()
+    memory = models.PositiveIntegerField()
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
+    product_year = models.PositiveIntegerField()
+    feature = models.ForeignKey(Feature, on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    is_active = models.BooleanField()
+
+
+class Image(models.Model):
+    products = models.ForeignKey(Products, on_delete=models.PROTECT)
+    is_main = models.BooleanField()
+    image = models.FileField(upload_to="products_image")
